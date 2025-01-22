91f43de34e4f48f1f76ef2bb24b75ca1e6a0f352
Ernesto Ndula <meteusrosa704@gmail.com>
2025-01-23 00:18:43 +0100

    Estruturamos pastas e apagamos arquivos

---
+++ src/books.js
@@ -0,0 +1,46 @@
+import img1 from './images/livro10.jpg';
+import img2 from './images/livro5.jpg';
+import img3 from './images/livro6.jpg';
+import img4 from './images/livro7.jpg';
+import img5 from './images/livro8.jpg';
+import img6 from './images/livro4.jpg';
+
+export const book = [
+    {
+        img: img1,
+        title: 'Elon Musk',
+        author: 'Walter Isaacson',
+        id: 1,
+    },
+    {
+        img: img2,
+        title: 'Hillbilly Elegy: A Memoir of a Family and Culture',
+        author: 'J. D. Vance',
+        id: 2,
+    },
+    {
+        img: img3,
+        title: 'Atomic Habits: An Easy & Proven Way to Build',
+        author: 'Jame Clear',
+        id: 3,
+    },
+    {
+        img: img4,
+        title: 'The Parable of the Sower (Parable, 1)',
+        author:'Octavia E. Butler',
+        id: 4,
+    },
+    {
+        img: img5,
+        title: 'Worthy: How to Believe You Are Enough and',
+        author: 'Jamie Kern Lima',
+        id: 5
+    },
+    {
+        img: img6,
+        title: 'Melania',
+        author: 'Melania Thrump',
+        id: 6
+    }
+
+]
